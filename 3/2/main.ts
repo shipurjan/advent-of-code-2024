1582bd08ea8a392e9f1ce11c473d05fd001fd31e
Cyprian Zdebski <cyprianz5mail@gmail.com>
2024-12-05 23:52:19 +0100

    add near-working 3b program

--- 3/2/main.ts
+++ 3/2/main.ts
@@ -1,3 +1,4 @@
+import { TextLineStream } from "jsr:@std/streams";
 import * as path from "jsr:@std/path";
 
 // ___________________________________ //
@@ -7,9 +8,59 @@ console.log(await computeAnswer(data));
 
 // ___________________________________ //
 
+interface Chunk {
+  index: number;
+  enabled: boolean;
+}
+
 async function computeAnswer(file: Deno.FsFile) {
-  void file;
-  return await Promise.resolve(null);
+  const readable = file.readable
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new TextLineStream());
+
+  let sum = 0;
+  for await (const line of readable) {
+    const toggleChunks = getToggleChunks(line);
+    const muls = [...line.matchAll(/mul\((?<X>\d{1,3}),(?<Y>\d{1,3})\)/g)].map(
+      (mul) => ({
+        ...mul,
+        enabled: toggleChunks.findLast((chunk) => chunk.index < mul.index)
+          ?.enabled ?? false,
+      }),
+    );
+
+    for (const mul of muls) {
+      if (!mul.enabled) continue;
+
+      const { X, Y } = mul.groups || {};
+      sum += Number(X) * Number(Y);
+    }
+  }
+  return sum;
+}
+
+function getInstructionIndices(line: string, regex: RegExp) {
+  return [...line.matchAll(new RegExp(regex.source, regex.flags))].map((
+    { index },
+  ) => index);
+}
+
+function getChunk(index: number, enabled: boolean): Chunk {
+  return ({
+    index,
+    enabled,
+  });
+}
+
+function getToggleChunks(line: string) {
+  // At the beginning of the program, mul instructions are enabled.
+  const enabled = [0, ...getInstructionIndices(line, /do\(\)/g)].map(
+    (i) => getChunk(i, true),
+  );
+  const disabled = getInstructionIndices(line, /don't\(\)/g).map(
+    (i) => getChunk(i, false),
+  );
+  return [...enabled, ...disabled].sort((a, b) => a.index - b.index);
 }
 
 async function openFile(filename: string) {
