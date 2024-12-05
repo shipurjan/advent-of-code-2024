4b8107e70164b57962edaae5030abbe0d3492dbc
Cyprian Zdebski <cyprianz5mail@gmail.com>
2024-12-05 21:22:10 +0100

    solve 3a

--- 3/1/main.ts
+++ 3/1/main.ts
@@ -1,3 +1,4 @@
+import { TextLineStream } from "jsr:@std/streams";
 import * as path from "jsr:@std/path";
 
 // ___________________________________ //
@@ -8,8 +9,24 @@ console.log(await computeAnswer(data));
 // ___________________________________ //
 
 async function computeAnswer(file: Deno.FsFile) {
-  void file;
-  return await Promise.resolve(null);
+  const readable = file.readable
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new TextLineStream());
+
+  let sum = 0;
+  for await (const line of readable) {
+    const matches = line.matchAll(/mul\((?<X>\d{1,3}),(?<Y>\d{1,3})\)/g);
+    for (const match of matches) {
+      const { X, Y } = match.groups || {};
+      if (X === undefined || Y === undefined) {
+        throw new Error(
+          `One of the inputs in the ${match[0]} expression is undefined`,
+        );
+      }
+      sum += Number(X) * Number(Y);
+    }
+  }
+  return sum;
 }
 
 async function openFile(filename: string) {
