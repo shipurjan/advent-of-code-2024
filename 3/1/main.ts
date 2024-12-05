fed9eb63d38df01e4f3c23b3793e8fb4fe741bc9
Cyprian Zdebski <cyprianz5mail@gmail.com>
2024-12-06 00:07:06 +0100

    simplify 3a

--- 3/1/main.ts
+++ 3/1/main.ts
@@ -1,36 +1,22 @@
-import { TextLineStream } from 'jsr:@std/streams'
 import * as path from 'jsr:@std/path'
 
 // ___________________________________ //
 
-const data = await openFile('data.txt')
+const data = await readTextFile('data.txt')
 console.log(await computeAnswer(data))
 
 // ___________________________________ //
 
-async function computeAnswer(file: Deno.FsFile) {
-  const readable = file.readable
-    .pipeThrough(new TextDecoderStream())
-    .pipeThrough(new TextLineStream())
-
-  let sum = 0
-  for await (const line of readable) {
-    const matches = line.matchAll(/mul\((?<X>\d{1,3}),(?<Y>\d{1,3})\)/g)
-    for (const match of matches) {
-      const { X, Y } = match.groups || {}
-      if (X === undefined || Y === undefined) {
-        throw new Error(
-          `One of the inputs in the ${match[0]} expression is undefined`,
-        )
-      }
-      sum += Number(X) * Number(Y)
-    }
-  }
-  return sum
+function computeAnswer(data: string) {
+  const matches = data.matchAll(/mul\((?<X>\d{1,3}),(?<Y>\d{1,3})\)/g)
+  return matches.reduce(
+    (prev, { groups: { X, Y } = {} }) => prev + Number(X) * Number(Y),
+    0,
+  )
 }
 
-async function openFile(filename: string) {
+async function readTextFile(filename: string) {
   const dirname = import.meta.dirname
   if (!dirname) throw new Error('Dirname is not defined for some reason')
-  return await Deno.open(path.join(dirname, filename))
+  return await Deno.readTextFile(path.join(dirname, filename))
 }
